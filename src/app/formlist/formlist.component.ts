0f04a99d93c69c3e311bd8db7ac6e247b394d4d4
siarotchi <stanislav.iarotchi@endava.com>
2020-05-26 22:36:28 +0300

    refactor req coments

--- src/app/formlist/formlist.component.ts
+++ src/app/formlist/formlist.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './formlist.component.html',
   styleUrls: ['./formlist.component.css'],
 })
-export class FormListComponent implements OnInit {
+export class FormListComponent {
   @Input() users: any;
   ngOnInit(): void {}
 }
